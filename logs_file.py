fa4538051dbe9a7ed0213b9c5bab5c85682bc3b8
Mohammad Javad Tatari <mjavadtatari98@gmail.com>
2021-04-21 10:28:37 +0430

    adding new files.

---
+++ logs_file.py
@@ -0,0 +1,37 @@
+import openpyxl
+import os
+
+logs_file_dir = 'administrator'
+logs_file = ''
+logs_file_worksheet = ''
+
+
+def open_logs_file():
+    # Try to open or create logs_file.xlsx
+
+    if not os.path.exists(logs_file_dir):
+        # Try to open or create directory.
+        os.makedirs(logs_file_dir)
+
+    if os.path.isfile(logs_file_dir + '\\logs_file.xlsx'):
+        # Try to find the xlsx file or create a new one.
+        # Opens and returns the worksheet.
+        logs_file = openpyxl.load_workbook(logs_file_dir + '\\logs_file.xlsx')
+        logs_file_worksheet = logs_file['system logs']
+    else:
+        logs_file = openpyxl.Workbook()
+        logs_file_worksheet = logs_file.active
+        logs_file_worksheet.title = 'system logs'
+
+    return logs_file_worksheet
+
+
+def save_logs_file():
+    # Saving the xlsx as logs_file.
+    logs_file.save(logs_file_dir + '\\logs_file.xlsx')
+
+    return True
+
+
+def last_line():
+    return logs_file_worksheet.max_row
---
+++ main.py
@@ -0,0 +1,4 @@
+import logs_file
+
+logs=logs_file.open_logs_file()
+
